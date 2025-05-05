6104433230e6f20962f41befe27d2bab853ccfbf
JasperLorelai <wolf.haci12@gmail.com>
2025-05-05 20:00:58 +0200

    refactor: Cleanup

--- src/Seconds.ts
+++ src/Seconds.ts
@@ -42,18 +42,16 @@ class SecondsIn {
     private static readonly MONTH = 30 * SecondsIn.DAY;
     private static readonly YEAR = 365 * SecondsIn.DAY;
 
-    static getSecondsIn(type: TimeTypes) {
-        switch (type) {
-            case TimeTypes.MILLISECOND: return SecondsIn.MILLISECOND;
-            case TimeTypes.SECOND: return SecondsIn.SECOND;
-            case TimeTypes.MINUTE: return SecondsIn.MINUTE;
-            case TimeTypes.HOUR: return SecondsIn.HOUR;
-            case TimeTypes.DAY: return SecondsIn.DAY;
-            case TimeTypes.WEEK: return SecondsIn.WEEK;
-            case TimeTypes.MONTH: return SecondsIn.MONTH;
-            case TimeTypes.YEAR: return SecondsIn.YEAR;
-        }
-    }
+    public static readonly MAP: Record<TimeTypes, number> = {
+        [TimeTypes.MILLISECOND]: SecondsIn.MILLISECOND,
+        [TimeTypes.SECOND]: SecondsIn.SECOND,
+        [TimeTypes.MINUTE]: SecondsIn.MINUTE,
+        [TimeTypes.HOUR]: SecondsIn.HOUR,
+        [TimeTypes.DAY]: SecondsIn.DAY,
+        [TimeTypes.WEEK]: SecondsIn.WEEK,
+        [TimeTypes.MONTH]: SecondsIn.MONTH,
+        [TimeTypes.YEAR]: SecondsIn.YEAR,
+    };
 
 }
 
@@ -61,84 +59,107 @@ export class Seconds {
 
     private readonly value: number;
 
-    constructor(seconds: number) {
+    /**
+     * Creates a {@link Seconds} object from the number of seconds passed.
+     * @param seconds amount of seconds
+     */
+    public constructor(seconds: number) {
         this.value = seconds;
     }
 
-    static from(timeType: TimeTypes, amount?: number) {
-        return new Seconds(SecondsIn.getSecondsIn(timeType) * (amount || 1));
+    /**
+     * Create a {@link Seconds} object from the number of time units passed.
+     * @param timeType {@link TimeTypes} unit of time
+     * @param amount amount of time units
+     * @see Seconds#milliseconds
+     * @see Seconds#seconds
+     * @see Seconds#minutes
+     * @see Seconds#days
+     * @see Seconds#weeks
+     * @see Seconds#months
+     * @see Seconds#years
+     */
+    public static from(timeType: TimeTypes, amount?: number) {
+        return new Seconds(SecondsIn.MAP[timeType] * (amount || 1));
     }
 
-    static milliseconds(amount: number = 1) {
+    public static milliseconds(amount: number = 1) {
         return this.from(TimeTypes.MILLISECOND, amount);
     }
 
-    static seconds(amount: number = 1) {
+    public static seconds(amount: number = 1) {
         return new Seconds(amount);
     }
 
-    static minutes(amount: number = 1) {
+    public static minutes(amount: number = 1) {
         return this.from(TimeTypes.MINUTE, amount);
     }
 
-    static hours(amount: number = 1) {
+    public static hours(amount: number = 1) {
         return this.from(TimeTypes.HOUR, amount);
     }
 
-    static days(amount: number = 1) {
+    public static days(amount: number = 1) {
         return this.from(TimeTypes.DAY, amount);
     }
 
-    static weeks(amount: number = 1) {
+    public static weeks(amount: number = 1) {
         return this.from(TimeTypes.WEEK, amount);
     }
 
-    static months(amount: number = 1) {
+    public static months(amount: number = 1) {
         return this.from(TimeTypes.MONTH, amount);
     }
 
-    static years(amount: number = 1) {
+    public static years(amount: number = 1) {
         return this.from(TimeTypes.YEAR, amount);
     }
 
-    milliseconds() {
+    public milliseconds() {
         return this.to(TimeTypes.MILLISECOND);
     }
 
-    seconds() {
+    public seconds() {
         return this.to(TimeTypes.SECOND);
     }
 
-    minutes() {
+    public toSeconds() {
+        return this.seconds();
+    }
+
+    public minutes() {
         return this.to(TimeTypes.MINUTE);
     }
 
-    hours() {
+    public hours() {
         return this.to(TimeTypes.HOUR);
     }
 
-    days() {
+    public days() {
         return this.to(TimeTypes.DAY);
     }
 
-    weeks() {
+    public weeks() {
         return this.to(TimeTypes.WEEK);
     }
 
-    months() {
+    public months() {
         return this.to(TimeTypes.MONTH);
     }
 
-    years() {
+    public years() {
         return this.to(TimeTypes.YEAR);
     }
 
-    toDuration() {
+    /**
+     * Creates a duration string from the {@link Seconds} object (e.g. "3 h, 4 min, 5 s ago").
+     */
+    public toDuration() {
         let sec = Math.abs(this.value);
         const elements: string[] = [];
 
         for (const type of TimeTypesOrdered) {
-            const unitSeconds = SecondsIn.getSecondsIn(type);
+            const unitSeconds = SecondsIn.MAP[type];
             const element = Math.floor(sec / unitSeconds);
             if (!element) continue;
             sec %= unitSeconds;
@@ -152,12 +173,19 @@ export class Seconds {
         return this.value < 0 ? `in ${duration}` : `${duration} ago`;
     }
 
-    to(timeType: TimeTypes) {
-        return timeType === TimeTypes.SECOND ? this.value : this.value / SecondsIn.getSecondsIn(timeType);
-    }
-
-    toSeconds() {
-        return this.to(TimeTypes.SECOND);
+    /**
+     * Get the amount of time stored in the {@link Seconds} object in the time unit passed.
+     * @param timeType Time unit
+     * @see Seconds#milliseconds
+     * @see Seconds#seconds
+     * @see Seconds#minutes
+     * @see Seconds#days
+     * @see Seconds#weeks
+     * @see Seconds#months
+     * @see Seconds#years
+     */
+    public to(timeType: TimeTypes) {
+        return timeType === TimeTypes.SECOND ? this.value : this.value / SecondsIn.MAP[timeType];
     }
 
 }
